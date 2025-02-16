0fc338826c0b29df789994bc188640e9ae3017d7
kirill <faceobito@yandex.ru>
2025-02-16 15:05:34 +0500

    add user interface

---
+++ Domain/Interfaces/IUser.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+
+namespace Domain.Interfaces;
+
+public interface IUser
+{
+    Task<IEnumerable<User>> GetAllUsers();
+    Task<User> GetUserById(Guid id);
+    Task<bool> CreateUser(User user);
+    Task<bool> DeleteUserById(Guid id);
+    Task<User> UpdateUser(User user);
+    
+    
+}
